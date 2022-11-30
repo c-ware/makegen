81da3ea0f9a44de8c186967f19f85ed8a2b9a927
HackerSmacker <>
2022-11-30 06:48:06 -0600

    fix: update docgen run command

--- scripts/load.sh
+++ scripts/load.sh
@@ -3,9 +3,16 @@
 # as dependencies. Although, this realistically should not be
 # ran by end users, and should instead be ran by developers.
 
-docgen project ./src/main.c --section cware  \
-                            --format manpage \
-                            --title 'C-Ware Manuals' \
-                            --date "`date +'%B %d, %Y'`"
+# This is for the now-deprecated single-executable docgen.
+# docgen project ./src/main.c --section cware  \
+#                             --format manpage \
+#                             --title 'C-Ware Manuals' \
+#                             --date "`date +'%B %d, %Y'`"
+
+# This is for the new multi-stage docgen.
+cat src/main.c | docgen-extractor-c | docgen-compiler-c | \
+	docgen-backend-manpage --section cware --title \
+	"C-Ware Manuals" --date "`date +'%B %d, %Y'`" \
+	> doc/makegen.cware
 
 makegen project unix --binary makegen --main src/main.c > Makefile
